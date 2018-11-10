2710301d962072c8cd8b5c06be3ca6d208e5c868
arnab majumdar <arnab156@gmail.com>
2018-11-10 09:32:31 -0600

    alertremoval

--- client/src/components/Profile/ProfilePg.js
+++ client/src/components/Profile/ProfilePg.js
@@ -11,6 +11,7 @@ class Profile extends Component {
 
   componentDidMount() {
     this.getBooksHandler();
+    this.getClubAndUSer();
   }
 
   getBooksHandler() {
@@ -27,30 +28,85 @@ class Profile extends Component {
       .catch(err => console.log(err));
   };
 
+  getClubAndUSer() {
+    myAPI
+    .searchAllClubs2()
+    .then(res =>
+      console.log(res.data)
+        // .forEach(function(e) {
+
+
+
+
+        //   e.clubMembers.forEach(function(user){
+
+        //     console.log(user);
+
+        //     if (user === localStorage.id_token){
+        //       //change the state of 
+        //       //show the club name on the page
+
+
+
+        //       // res.data.forEach(function (club){
+        //       //   console.log(club)
+        //       // })
+
+
+
+
+        //     }
+        //   })
+
+
+
+
+
+
+
+
+
+
+        // })
+
+    
+        
+    ).catch(err => console.log(err))
+  }
+
   render() {
-    let saved = <p>There are no books saved! Go to Search Book page to find a book of your choice</p>;
+    let saved = (
+      <p>
+        There are no books saved! Go to Search Book page to find a book of your
+        choice
+      </p>
+    );
 
     if (this.state.savedBooks.length > 0) {
       saved = this.state.savedBooks.map((book, index) => {
-        console.log(book);
+        // console.log(book);
         return (
-          
-            <BookScraped
-              key={book._id}
-              bookId={book._id}
-              title={book.title}
-              author={book.author}
-              URL={book.URL}
-              preview={book.preview}
-              action={this.deleteBooksHandler}
-              saveTitle="Delete from saved"
-            />
-         
+          <BookScraped
+            key={book._id}
+            bookId={book._id}
+            title={book.title}
+            author={book.author}
+            URL={book.URL}
+            preview={book.preview}
+            action={this.deleteBooksHandler}
+            saveTitle="Delete from saved"
+          />
         );
       });
     }
 
-    let savedClubs = <p> No Clubs Saved, go to BookClubs page to search, create and join a book club</p>
+    let savedClubs = (
+      <p>
+        {" "}
+        No Clubs Saved, go to BookClubs page to search, create and join a book
+        club
+      </p>
+    );
 
     return (
       <div>
@@ -58,14 +114,13 @@ class Profile extends Component {
         <hr />
         <hr />
 
-      <h1>Saved Clubs</h1>
-        <br/>
+        <h1>Saved Clubs</h1>
+        <br />
         {savedClubs}
-<br/>
-
+        <br />
 
         <h1>Saved Books</h1>
-        <br/>
+        <br />
         {saved}
       </div>
     );
--- routes/api/bookClubs.js
+++ routes/api/bookClubs.js
@@ -3,7 +3,8 @@ const bookClubsController = require("../../controllers/bookClubsController");
 const db = require("../../models");
 
 // Matches with "/api/books"
-router.route("/")
+router
+  .route("/")
   .get(bookClubsController.findAll)
   .post(bookClubsController.create);
 
@@ -14,60 +15,65 @@ router
   .put(bookClubsController.update)
   .delete(bookClubsController.remove);
 
+router.post("/searchall", function(req, res) {
+  // console.log(req.body.searchByBookNameByBook, "This is the search object");
 
-  router.post('/searchall', function(req, res){
-    // console.log(req.body.searchByBookNameByBook, "This is the search object");
-    
-    db.Club.find({_id : req.body.clubid})
-    .then(result => {
-      console.log(result);
-      res.send(result);
-    })  
+  db.Club.find({ _id: req.body.clubid }).then(result => {
+    // console.log(result);
+    res.send(result);
+  });
+});
+
+router.post("/searchall2", function(req, res) {
+  // console.log(req.body.searchByBookNameByBook, "This is the search object");
+
+  db.Club.find({  }).then(result => {
+    console.log(result);
+    res.send(result);
+  });
 });
 
 
+router.post("/search", function(req, res) {
+  // console.log(req.body.searchByBookNameByBook, "This is the search object");
 
-
-router.post('/search', function(req, res){
-    // console.log(req.body.searchByBookNameByBook, "This is the search object");
-    
-    db.Club.find({bookName:req.body.searchByBookNameByBook })
-    .then(result => {
-      console.log(result);
-      res.send(result);
-    })  
+  db.Club.find({ bookName: req.body.searchByBookNameByBook }).then(result => {
+    // console.log(result);
+    res.send(result);
+  });
 });
 
-router.post('/search2', function(req, res){
+router.post("/search2", function(req, res) {
   // console.log(req.body.searchClubName, "This is the search object in bookclub.js");
-  
-  db.Club.find({clubName:req.body.searchClubName })
-  .then(result => {
+
+  db.Club.find({ clubName: req.body.searchClubName }).then(result => {
     // console.log( "line 34", result);
     res.send(result);
-  })
+  });
 });
 
-router.post('/addClub', function(req, res){
+router.post("/addClub", function(req, res) {
   // console.log(req.body.searchClubName, "This is the search object in bookclub.js *%%*&%^E*^E");
   // console.log(req.body, "This is the search object in bookclub.js in line 40 bookclub.js");
-  db.Club.findOneAndUpdate({_id: req.body.clubid},{"$push": { "clubMembers": req.body.useradd }})
-
+  db.Club.findOneAndUpdate(
+    { _id: req.body.clubid },
+    { $push: { clubMembers: req.body.useradd } }
+  )
   .then(result => {
     // console.log( "line 34", result);
     res.send(result);
-  })
+  });
 });
 
-router.post('/discussionadd', function(req, res){
-  db.Club.findOneAndUpdate({_id: req.body.clubid},{"$push": { "discussions": req.body.discussionadd }})
-
+router.post("/discussionadd", function(req, res) {
+  db.Club.findOneAndUpdate(
+    { _id: req.body.clubid },
+    { $push: { discussions: req.body.discussionadd } }
+  )
   .then(result => {
     // console.log( "line 34", result);
     res.send(result);
-  })
+  });
 });
-
-
 
 module.exports = router;
